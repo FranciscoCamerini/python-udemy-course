cd4d254675ee820433a4ca8231884965b80cf4b7
Francisco Camerini <83650761+FranciscoCamerini@users.noreply.github.com>
2022-03-18 09:37:29 -0300

    Add files via upload

---
+++ groupby/main.py
@@ -0,0 +1,28 @@
+from itertools import groupby, tee
+
+alunos = [
+    {'nome': 'Luiz', 'nota': 'A'},
+    {'nome': 'Leticia', 'nota': 'B'},
+    {'nome': 'Fabricio', 'nota': 'A'},
+    {'nome': 'Rosemary', 'nota': 'C'},
+    {'nome': 'Joana', 'nota': 'D'},
+    {'nome': 'João', 'nota': 'A'},
+    {'nome': 'Eduardo', 'nota': 'B'},
+    {'nome': 'Andre', 'nota': 'A'},
+    {'nome': 'Anderson', 'nota': 'C'},
+    {'nome': 'José', 'nota': 'B'}
+]
+ordena = lambda item: item['nota']
+alunos.sort(key=ordena)
+alunos_agrupados = groupby(alunos, ordena)
+
+for agrupamento, valores_agrupados in alunos_agrupados:
+    va1, va2 = tee(valores_agrupados)
+    print(f'Agrupamento: {agrupamento}')
+
+    for aluno in va1:
+        print(f'\t{aluno}')
+
+    quantidade = len(list(va2))
+    print(f'\t{quantidade} alunos tiraram a nota {agrupamento}')
+    print()
