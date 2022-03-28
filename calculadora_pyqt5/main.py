eb0c98ad6e41642a119630ff0343b1cc6a305c07
Francisco Camerini <83650761+FranciscoCamerini@users.noreply.github.com>
2022-03-28 17:18:49 -0300

    Update main.py

--- calculadora_pyqt5/main.py
+++ calculadora_pyqt5/main.py
@@ -1,3 +1,7 @@
+#
+#    Criando uma calculadora usando PyQT5
+#
+
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QLineEdit
